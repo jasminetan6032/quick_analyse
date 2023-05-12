496abd59543cd7de5530c96a0a300e41eb593e78
Jasmine Tan <jasmine.tan6032@gmail.com>
2023-05-11 20:13:57 -0400

    code for analysing data immediately after collection. Checks triggers, accuracy and reaction time

---
+++ quick_analyse.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  9 12:05:43 2023
+
+@author: jwt30
+"""
+import mne 
+import pandas
+import numpy
+import glob
+import quick_analyse_config as cfg
+from termcolor import colored
+
+def find_response_triggers(events):
+    events_data = pandas.DataFrame(events)
+    events_data.columns = ['sample', 'initialState','trigger']
+    trigger_counts = events_data['trigger'].value_counts()
+    #check for largest number of triggers (leave case for those that are missing one side - eg. participant 075801)
+    likely_response_triggers = trigger_counts[trigger_counts.index > 255].nlargest(2).index
+    
+    if len(likely_response_triggers) == 2:
+        if numpy.any(likely_response_triggers[0] == cfg.right_responses):
+            cfg.event_dict.update({'response/right':likely_response_triggers[0]})
+            cfg.event_dict.update({'response/left':likely_response_triggers[1]})
+        elif numpy.any(likely_response_triggers[0] == cfg.left_responses):
+            cfg.event_dict.update({'response/right':likely_response_triggers[1]})
+            cfg.event_dict.update({'response/left':likely_response_triggers[0]})
+
+    elif len(likely_response_triggers) == 1:
+        if numpy.any(likely_response_triggers[0] == cfg.right_responses):
+            cfg.event_dict.update({'response/right':likely_response_triggers[0]})
+            print("Warning: Missing left response")
+
+        elif numpy.any(likely_response_triggers[0] == cfg.left_responses):
+            cfg.event_dict.update({'response/left':likely_response_triggers[0]})
+            print("Warning: Missing right response")
+            
+    elif len(likely_response_triggers) == 0:
+        print("Warning: Missing all response triggers")
+
+              
+
+def check_triggers(events):
+    #check for number of stimuli and response triggers. If it doesn't equal to 288 for AttenAud and 168 for AttenVis (per run), or if there are an unreasonable number of response triggers, flag to take a look.
+    stimuli_list = events[:, 2].tolist()
+    list_of_stimuli_triggers = [i for i in stimuli_list if i < 255]
+    if len(list_of_stimuli_triggers) != cfg.number_of_stimuli:
+        warning = 'Unexpected number of stimuli triggers: '
+        print(colored(warning + str(len(list_of_stimuli_triggers)),'magenta'))
+
+    #check column two for any triggers that do not start from zero and extract events to eyeball
+    non_zero_initial_states = [i for i in range(0, len(events)) if not events[i,1]==0]
+    problematic_events = events[non_zero_initial_states,:]
+    return problematic_events
+
+
+def get_metadata_and_behaviour(events):
+    row_events = [event_name for event_name in list(cfg.event_dict.keys()) if not 'response' in event_name]
+    keep_first = ["response"]
+    metadata, events, event_id = mne.epochs.make_metadata(
+        events=events, event_id=cfg.event_dict,
+        tmin=cfg.metadata_tmin, tmax=cfg.metadata_tmax, sfreq=raw.info['sfreq'],
+        row_events=row_events,
+        keep_first=keep_first)
+    
+    if cfg.paradigm in ('AttenAud','Misophonia'):
+        
+        metadata[['attention_side','stimuli_type', 'pitch','stimulus_side']] = metadata.event_name.str.split('/',expand = True)
+        metadata.loc[(metadata['first_response'] == metadata['stimulus_side']) & (metadata['stimuli_type'].str.contains('target')),
+                     'response_correct'] = 'Hit'
+        metadata.loc[(metadata['first_response'] != metadata['stimulus_side']) & (metadata['stimuli_type'].str.contains('target')),
+                     'response_correct'] = 'Incorrect Response'
+        metadata.loc[~(metadata['first_response'].isnull()) & ~(metadata['stimuli_type'].str.contains('target')),
+                     'response_correct'] = 'False Alarm'
+        metadata.loc[(metadata['first_response'].isnull()) & (metadata['stimuli_type'].str.contains('target')),
+                     'response_correct'] = 'Missed Target'
+        metadata.loc[(metadata['first_response'].isnull()) & ~(metadata['stimuli_type'].str.contains('target')),
+                     'response_correct'] = 'Correct Rejection'
+        
+        stimuli_counts = metadata.value_counts(['stimuli_type','stimulus_side','attention_side'], sort = False)
+        
+        if not set(cfg.stimuli_count_reference).issubset(stimuli_counts.index):
+            stimuli_counts= stimuli_counts.reindex(cfg.stimuli_count_reference,fill_value=0)
+        
+        stimuli_counts = stimuli_counts.sort_index()
+        
+        response_counts = metadata['response_correct'].value_counts()
+        
+        if not set(cfg.response_count_reference).issubset(response_counts.index):
+            response_counts= response_counts.reindex(cfg.response_count_reference,fill_value=0)
+    
+        
+        average_performance_in_run = pandas.DataFrame([{"%correct": round(response_counts['Hit']/stimuli_counts['target'].sum(),2), "reaction_time":metadata['response'].median()}])
+        
+        if cfg.paradigm == 'AttenAud':
+            run_info = pandas.DataFrame([{"Stds_on_R_Attend_R":stimuli_counts['standard','right','attendRight'],"Stds_on_L_Attend_L":stimuli_counts['standard','left','attendLeft'],
+                        "Beeps_on_R_Attend_L":stimuli_counts['beep','right','attendLeft'],"Beeps_on_L_Attend_R":stimuli_counts['beep','left','attendRight'],
+                        "Targets_on_R_Attend_R":stimuli_counts['target','right'].sum(),"Targets_on_L_Attend_L":stimuli_counts['target','left'].sum(),
+                        "Novels_on_R_Attend_L":stimuli_counts['novel','right'].sum(),"Novels_on_L_Attend_R":stimuli_counts['novel','left'].sum(),
+                        "Distractors_on_R_Attend_L":stimuli_counts['dev','right'].sum(),"Distractors_on_L_Attend_R":stimuli_counts['dev','left'].sum(),
+                        "Hits": response_counts['Hit'], "FalseAlarms": response_counts['False Alarm'], "IncorrectResponses":response_counts['Incorrect Response'],
+                        "Misses":response_counts['Missed Target'],"CorrectRejections":response_counts['Correct Rejection'],
+                        "nStimuli":len(metadata),"nResponses":metadata['response'].notna().sum()}])
+        
+        elif cfg.paradigm == 'Misophonia':
+            run_info = pandas.DataFrame([{"Stds_on_R_Attend_R":stimuli_counts['standard','right','attendRight'],"Stds_on_L_Attend_L":stimuli_counts['standard','left','attendLeft'],
+                        "Beeps_on_R_Attend_L":stimuli_counts['beep','right','attendLeft'],"Beeps_on_L_Attend_R":stimuli_counts['beep','left','attendRight'],
+                        "Targets_on_R_Attend_R":stimuli_counts['target','right'].sum(),"Targets_on_L_Attend_L":stimuli_counts['target','left'].sum(),
+                        "Novels_on_R_Attend_L":stimuli_counts['novel','right'].sum(),"Novels_on_L_Attend_R":stimuli_counts['novel','left'].sum(),
+                        "Distractors_on_R_Attend_L":stimuli_counts['dev','right'].sum(),"Distractors_on_L_Attend_R":stimuli_counts['dev','left'].sum(),
+                        "Misophones_on_R_Attend_L":stimuli_counts['misophone','right'].sum(),"Misophones_on_L_Attend_R":stimuli_counts['misophone','right'].sum(),
+                        "Hits": response_counts['Hit'], "FalseAlarms": response_counts['False Alarm'], "IncorrectResponses":response_counts['Incorrect Response'],
+                        "Misses":response_counts['Missed Target'],"CorrectRejections":response_counts['Correct Rejection'],
+                        "nStimuli":len(metadata),"nResponses":metadata['response'].notna().sum()}])
+        
+    elif cfg.paradigm == 'AttenVis':
+        metadata[['condition','difficulty']] = metadata.event_name.str.split('/',expand = True)
+        metadata.loc[metadata['event_name'] == 'target',
+                     'response'] = float("nan")
+        metadata.loc[metadata['event_name'] == 'target',
+                     'first_response'] = 'None'
+        stimuli_counts = metadata.value_counts(['condition','difficulty'], sort = False)
+        response_counts = metadata['first_response'].value_counts()
+        run_info = pandas.DataFrame([{"pop_out4": stimuli_counts['pop-out','4'],"pop_out6": stimuli_counts['pop-out','6'], 
+                    "pop_out8": stimuli_counts['pop-out','8'], "pop_out10": stimuli_counts['pop-out','10'],
+                    "search4": stimuli_counts['search','4'],"search6": stimuli_counts['search','6'], 
+                    "search8": stimuli_counts['search','8'], "search10": stimuli_counts['search','10'],
+                    "total_pop-outs": stimuli_counts['pop-out'].sum(),"total_search": stimuli_counts['search'].sum(), 
+                    "right_responses": response_counts['right'], "left_responses": response_counts['left'],
+                    "nStimuli": stimuli_counts.sum(),"nResponses":metadata['response'].notna().sum()}])
+        average_performance_in_run = pandas.DataFrame([{"reaction_time":metadata['response'].median()}])
+   
+    return run_info, average_performance_in_run
+
+
+
+summary = pandas.DataFrame()
+average_performance = pandas.DataFrame()
+
+files = glob.glob(cfg.data_dir + str(cfg.participant) + '_' +cfg.paradigm + '_run0*_raw.fif')
+files.sort()
+
+for file in files:
+    raw = mne.io.read_raw_fif(file)
+    try:
+        events = mne.find_events(raw, stim_channel='STI101')
+    except:
+        events = mne.find_events(raw, stim_channel='STI101',shortest_event=1)
+        print(colored("Warning: ValueError:shortest event length = 1",'yellow'))
+    find_response_triggers(events)
+    problematic_events = check_triggers(events)
+    if problematic_events.size >0:
+        print(problematic_events)
+  
+    run_info,average_performance_in_run = get_metadata_and_behaviour(events)
+    average_performance = pandas.concat([average_performance,average_performance_in_run])
+    summary = pandas.concat([summary,run_info])
+
+print(colored(summary.sum(),'cyan'))
+print(average_performance.mean())
---
+++ quick_analyse_config.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  9 15:03:31 2023
+
+@author: jwt30
+"""
+import os
+import pandas
+
+paradigm = 'AttenAud'
+participant = '075801'
+date = 230311
+
+MEG_dir = '/autofs/space/megraid_research/MEG/tal'
+
+data_dir = os.path.join(MEG_dir,'subj_'+ participant,str(date)+'/')
+
+correct_stimuli_triggers = {'AttenVis': 168*2, #targets and stimuli grids
+                            'AttenAud': 288,
+                            'Misophonia': 288}
+
+number_of_stimuli = correct_stimuli_triggers[paradigm]
+
+tmin_tmax = {'AttenVis': {'tmin':0.0,
+                          'tmax':2.0},
+             'AttenAud': {'tmin':0.0,
+                          'tmax':1.0},
+             'Misophonia': {'tmin':0.0,
+                            'tmax':1.0}}
+
+metadata_tmin, metadata_tmax = tmin_tmax[paradigm]['tmin'], tmin_tmax[paradigm]['tmax']
+
+right_responses = [256,512,1024,2048]
+left_responses = [4096,8192,16384,32768]
+all_responses = right_responses + left_responses
+
+all_event_dicts = {
+
+    'AttenVis' : {'search/4': 1,
+                     'search/6': 2,
+                     'search/8': 3,
+                     'search/10': 4,
+                     'pop-out/4': 5,
+                     'pop-out/6': 6,
+                     'pop-out/8': 7,
+                     'pop-out/10': 8,
+                     'target': 32,
+                     'response/right': 2048,
+                     'response/left': 32768
+                     },
+
+    'AttenAud' : {'attendRight/standard/high/right': 1,
+                     'attendRight/standard/low/right': 3,
+                     'attendRight/target/high/right': 11,
+                     'attendRight/target/low/right': 13,
+                     'attendRight/beep/low/left': 5,
+                     'attendRight/beep/high/left': 7,
+                     'attendRight/dev/low/left': 35,
+                     'attendRight/dev/high/left': 37,
+                     'attendRight/novel/low/left': 25,
+                     'attendRight/novel/high/left': 27,
+                     'attendLeft/standard/high/left': 2,
+                     'attendLeft/standard/low/left': 4,
+                     'attendLeft/target/high/left': 12,
+                     'attendLeft/target/low/left': 14,
+                     'attendLeft/beep/low/right': 6,
+                     'attendLeft/beep/high/right': 8,
+                     'attendLeft/dev/low/right': 36,
+                     'attendLeft/dev/high/right': 38,
+                     'attendLeft/novel/low/right': 26,
+                     'attendLeft/novel/high/right': 28,
+                     }, 
+
+    'Misophonia': {'attendRight/standard/high/right': 1,
+                     'attendRight/standard/low/right': 3,
+                     'attendRight/target/high/right': 11,
+                     'attendRight/target/low/right': 13,
+                     'attendRight/beep/low/left': 5,
+                     'attendRight/beep/high/left': 7,
+                     'attendRight/dev/low/left': 35,
+                     'attendRight/dev/high/left': 37,
+                     'attendRight/novel/low/left': 25,
+                     'attendRight/novel/high/left': 27,
+                     'attendRight/misophone/low/left': 45,
+                     'attendRight/misophone/high/left': 47,
+                     'attendLeft/standard/high/left': 2,
+                     'attendLeft/standard/low/left': 4,
+                     'attendLeft/target/high/left': 12,
+                     'attendLeft/target/low/left': 14,
+                     'attendLeft/beep/low/right': 6,
+                     'attendLeft/beep/high/right': 8,
+                     'attendLeft/dev/low/right': 36,
+                     'attendLeft/dev/high/right': 38,
+                     'attendLeft/novel/low/right': 26,
+                     'attendLeft/novel/high/right': 28,
+                     'attendLeft/misophone/low/right': 46,
+                     'attendLeft/misophone/high/right': 48
+                     }  
+    }
+
+event_dict = all_event_dicts[paradigm]
+
+stimuli_type = ['beep','beep','dev','dev','novel','novel','standard','standard','target','target']
+stimuli_type_misophonia = stimuli_type + ['misophone','misophone']
+ 
+stimulus_side = ['left','right','left','right','left','right','right','left','right','left']
+stimulus_side_misophonia = stimulus_side + ['left','right']
+
+attention_side=['attendRight','attendLeft','attendRight','attendLeft','attendRight','attendLeft','attendRight','attendLeft','attendRight','attendLeft']
+attention_side_misophonia= attention_side + ['attendRight','attendLeft'] 
+
+if paradigm == 'AttenAud':
+    stimuli_count_reference = pandas.MultiIndex.from_arrays([stimuli_type,stimulus_side,attention_side], names=('stimuli_type', 'stimulus_side','attention_side'))
+elif paradigm == 'Misophonia':
+    stimuli_count_reference = pandas.MultiIndex.from_arrays([stimuli_type_misophonia,stimulus_side_misophonia,attention_side_misophonia], names=('stimuli_type', 'stimulus_side','attention_side'))
+    
+response_count_reference = ['Correct Rejection','Hit','False Alarm','Incorrect Response','Missed Target']

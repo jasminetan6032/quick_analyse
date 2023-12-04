7cf60e3245102364be4fa0332c1f4643a0bbf68f
Jasmine Tan <jasmine.tan6032@gmail.com>
2023-12-04 12:24:02 -0500

    clean up miso answers

--- find_miso_sounds.py
+++ find_miso_sounds.py
@@ -6,15 +6,13 @@ Created on Tue Oct  3 13:22:44 2023
 @author: jwt30
 """
 import pandas as pd
-from tkinter.filedialog import askopenfilename
 from termcolor import colored
 
-
+#make sure you always have an updated file from RedCap called MisophoniaAudioRatings in quick_analyse
 filepathname = '/local_mount/space/hypatia/2/users/Jasmine/github/quick_analyse/MisophoniaAudioRatings.csv'
 df = pd.read_csv(filepathname, dtype=str)
 
 participant_number = input("Enter participant number: ")
-print(participant_number)
 
 df = df[df['Event Name']=='Testing 1']
 df = df.drop(columns = "Event Name")
@@ -25,10 +23,10 @@ data_long = pd.wide_to_long(participant_df,["Please identify this sound (guess i
                             "Subject ID:","Sound to identify",sep = ".")
 data_long = data_long.rename(columns = {"Please identify this sound (guess if you are unsure)": "Identified sound",
                             "Please rate the intensity of your reaction to this sound when made by another person or object": "Rating"})
-answers = ["lipsmack2","swallow2","sniffing","slurp2","jointcrack3","throat clear","breathing3","bang","nailclip2","bark","chain","chewing", 
-           "chomping","cling","clong","dial tone","honk","raygun","snoring","breathing1","jointcrack5","lipsmack3","nailclip4","slurp1","swallow1"]
-miso_or_novel = [1,1,1,1,1,1,1,0,1,0,0,1,
-                 1,0,0,0,0,0,1,1,1,1,1,1,1]
+answers = ["lipsmack2","sniffing","throat clear","bang","nailclip2","bark","chain","chewing", 
+           "cling","clong","dial tone","honk","raygun","snoring","breathing1","jointcrack5","slurp1","swallow1"]
+miso_or_novel = [1,1,1,0,1,0,0,1,
+                 0,0,0,0,0,1,1,1,1,1]
 data_long["Correct answer"] = answers
 data_long["Misophonic trigger"] = miso_or_novel
 data_long["Rating"] = pd.to_numeric(data_long["Rating"])
@@ -43,6 +41,7 @@ bottom3_novels_avg = novels["Rating"][-3:].mean()
 
 if top5_miso_avg - bottom3_novels_avg > 5:
     print("Participant has valid sounds for experiment")
+else: print(colored("Participant does NOT have valid sounds for experiment",'red'))
     
 top5miso = miso_sounds[['Identified sound','Rating', 'Correct answer']].head(5)
 print(colored(top5miso,'cyan'))
